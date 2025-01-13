3cd2d685621b4c6fa5b99498167702c646b16c27
Mosses <mosses@cloudiotech.com>
2025-01-13 09:35:50 +0530

    Minimum Viable Fields are added

--- src/FileCabinet/SuiteScripts/xcsd-mv-cs.js
+++ src/FileCabinet/SuiteScripts/xcsd-mv-cs.js
@@ -1,1 +1,22 @@
 
+/**
+ * @NApiVersion 2.x
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+
+/*
+Name          : Client Script for Button Action
+Author        : Mosses
+Description   : Handles button click to increment a field and submit the form.
+Dependencies  : N/currentRecord
+Release Date  : 2025-01-09
+Version       : 1.0.0
+Changing      : 1.0.0 - Initial release
+Website       : www.cloudio.com
+*/
+
+define(['N/currentRecord'], main);
+
+function main (currentRecord) {   
+}
